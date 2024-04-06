6b6bac9a9d0de6c46b867f595fb00cd6082caeaf
avni0405 <avinavekariya6@gmail.com>
2024-04-06 14:46:07 +0530

    Added html code

--- Chatbot/routes.py
+++ Chatbot/routes.py
@@ -1,4 +1,4 @@
-from quart import send_file, Blueprint
+from quart import send_file, Blueprint, render_template
 import asyncio
 import aiohttp
 from aiohttp import ClientTimeout
@@ -13,7 +13,8 @@ import time
 from moviepy.editor import VideoFileClip, concatenate_videoclips
 import speech_recognition as sr
 from openai import OpenAI
-import os
+import pytrends
+from pytrends.request import TrendReq
 
 bp = Blueprint("routes", __name__)
 
@@ -39,18 +40,18 @@ async def site_details(session, url):
         try:
             soup = BeautifulSoup(html, 'html.parser')
             title = soup.title.text if soup.title else "Title not found"
-            
+
             # Get meta description
             metas = soup.find_all('meta')
             description = [meta.attrs['content'] for meta in metas if
                            'name' in meta.attrs and meta.attrs['name'] == 'description']
             description = description[0] if description else ""
-            
+
             # Get og:image
             og_image = [meta.attrs['content'] for meta in metas if
                         'property' in meta.attrs and meta.attrs['property'] == 'og:image']
             og_image = og_image[0] if og_image else ""
-            
+
             title_with_link = f"• <b><a href='{url}' target='_blank'>{title}</a></b>"
             return {'url': url, 'title': title_with_link, 'description': description, 'og_image': og_image}
         except Exception as e:
@@ -66,34 +67,57 @@ async def search_google_async(query, num_results=10, lang="en"):
         return await asyncio.gather(*tasks)
 
 
+@bp.route('/result', methods=['GET'])
+async def show_result():
+    result = {
+        "searches": [
+            {
+                "title": "wikipedia",
+                "description": "this is wikipedia"
+            },
+            {
+                "title": "wikipedia1",
+                "description": "this is wikipedia"
+            },
+            {
+                "title": "wikipedia2",
+                "description": "this is wikipedia"
+            },
+            {
+                "title": "wikipedia3",
+                "description": "this is wikipedia"
+            },
+        ]
+    }
+
+    return await render_template('results.html', result=result)
+
+
 @bp.route('/search', methods=['POST'])
 async def search_google():
     user_message = (await request.json)['message']
     if check_matching_phrase(user_message):
-        results_with_snippets="Hello! I am feeling wonderfull today. How can I assist you today?"
+        results_with_snippets = "Hello! I am feeling wonderfull today. How can I assist you today?"
     else:
         results_with_snippets = await search_google_async(user_message)
-
-    # Save the results to an HTML file
-    await save_results_to_html(results_with_snippets, user_message)
-    print("Vaishnavi   ",{'results': results_with_snippets})
     # Return the JSON response
     return jsonify({'results': results_with_snippets})
 
 
 async def save_results_to_html(results, user_message):
+    pass
     # Create or open the HTML file for writing with UTF-8 encoding
-    with open('results.html', 'w', encoding='utf-8') as f:
-        # Write the HTML content to the file
-        # user_message = (await request.json)['message']
-        title_res = "Your Search: " + user_message
-        f.write(
-            '<!DOCTYPE html>\n<html lang="en" data-bs-theme="dark">\n<head>\n<meta charset="UTF-8">\n<meta http-equiv="X-UA-Compatible" content="IE=edge">\n<meta name="viewport" content="width=device-width, initial-scale=1.0">\n<link rel="stylesheet" href="style.css">\n <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">\n<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />\n<link rel="preconnect" href="https://fonts.googleapis.com">\n<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>\n<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap" rel="stylesheet">\n<title>Chalaang Queens</title>\n<body><nav class="navbar">\n<a class="navbar-brand" href="#">\n<img src="img\logo.png" width="40" height="40" alt="">\n</a>\n<a class="navbar-brand" href="#"><b>Chalaang-Queens</b></a>\n<ul class="nav">\n<li class="nav-item">\n<a class="nav-link active" aria-current="page" href="index.html">Home</a>\n</li>\n<li class="nav-item">\n<a class="nav-link" href="#">Your Search</a>\n</li>\n</ul>\n</nav>\n<h1><center>{}</center></h1>\n'.format(
-                title_res))
-        for result in results:
-            f.write(
-                f"<li><a href='{result['url']}' target='_blank'>{result['title']}</a><br>{result['description']}</li>\n")
-        f.write('</ul>\n</body>\n</html>')
+    # with open('results.html', 'w', encoding='utf-8') as f:
+    #     # Write the HTML content to the file
+    #     # user_message = (await request.json)['message']
+    #     title_res = "Your Search: " + user_message
+    #     f.write(
+    #         '<!DOCTYPE html>\n<html lang="en" data-bs-theme="dark">\n<head>\n<meta charset="UTF-8">\n<meta http-equiv="X-UA-Compatible" content="IE=edge">\n<meta name="viewport" content="width=device-width, initial-scale=1.0">\n<link rel="stylesheet" href="style.css">\n <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">\n<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />\n<link rel="preconnect" href="https://fonts.googleapis.com">\n<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>\n<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap" rel="stylesheet">\n<title>Chalaang Queens</title>\n<body><nav class="navbar">\n<a class="navbar-brand" href="#">\n<img src="img\logo.png" width="40" height="40" alt="">\n</a>\n<a class="navbar-brand" href="#"><b>Chalaang-Queens</b></a>\n<ul class="nav">\n<li class="nav-item">\n<a class="nav-link active" aria-current="page" href="index.html">Home</a>\n</li>\n<li class="nav-item">\n<a class="nav-link" href="#">Your Search</a>\n</li>\n</ul>\n</nav>\n<h1><center>{}</center></h1>\n'.format(
+    #             title_res))
+    #     for result in results:
+    #         f.write(
+    #             f"<li><a href='{result['url']}' target='_blank'>{result['title']}</a><br>{result['description']}</li>\n")
+    #     f.write('</ul>\n</body>\n</html>')
 
 
 def video_audio_overlay():
@@ -143,7 +167,7 @@ def assistant(audio):
     voices = engine.getProperty('voices')
     engine.setProperty('voice', voices[1].id)
     engine.setProperty('rate', 130)
-    engine.save_to_file(audio, r'img\testtemp.wav')
+    engine.save_to_file(audio, r'../static/img/testtemp.wav')
     engine.runAndWait()
     video_audio_overlay()
     # Call the function to change video src
@@ -158,28 +182,33 @@ async def change_video_src_handler():
 
     return await change_video_src()
 
+
 def check_matching_phrase(input_text):
     phrases = [
-    "Hi",
-    "how are you?",
-    "how are you",
-    "Hi, how are you?",
-    "What's up?",
-    "How's it going?",
-    "Good morning!",
-    "Hey there!"
+        "Hi",
+        "Hello",
+        "how are you?",
+        "how are you",
+        "Hi, how are you?",
+        "What's up?",
+        "How's it going?",
+        "Good morning!",
+        "Hey there!"
     ]
     for phrase in phrases:
         if input_text.lower() == phrase.lower():
             return True
     return False
 
+
 def summarize_content(content):
     client = OpenAI(
-    api_key="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",
+        api_key="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",
     )
-    prompt_text = [{"role":"system","content":"You are a serach engine.Summarize/analyze webpages to give comprehensive information"},{"role":"user","content":content}]
-    response = client.completions.create(        
+    prompt_text = [{"role": "system",
+                    "content": "You are a serach engine.Summarize/analyze webpages to give comprehensive information"},
+                   {"role": "user", "content": content}]
+    response = client.completions.create(
         model="gpt-3.5-turbo-instruct",
         prompt=prompt_text,
         max_tokens=1000,  # Adjust the max tokens to control the length of the summary
@@ -187,7 +216,7 @@ def summarize_content(content):
         top_p=1.0,
         frequency_penalty=0.0,
         presence_penalty=0.0,
-        
+
     )
     summary = response.choices[0].text.strip()
     return summary
--- Chatbot/static/js/script.js
+++ Chatbot/static/js/script.js
@@ -1,75 +1,88 @@
 const messageBar = document.querySelector(".bar-wrapper input");
 const sendBtn = document.querySelector(".bar-wrapper button");
 const messageBox = document.querySelector(".message-box");
-
+let i =0
 const API_URL = "http://localhost:5000/search"; // Update with your Flask API endpoint
 
 // Function to send message
 const sendMessage = async () => {
-  if (messageBar.value.length > 0) {
-    const userTypedMessage = messageBar.value;
-    messageBar.value = "";
+    if (messageBar.value.length > 0) {
+        const userTypedMessage = messageBar.value;
+        messageBar.value = "";
 
-    let message = `
+        let message = `
       <div class="chat1 message">
-        <img src="img/user.jpg">
+        <img src="../static/img/user.jpg">
         <span>
           ${userTypedMessage}
         </span>
       </div>`;
 
-    let response = `
+        let response = `
       <div class="chat response">
-        <img src="img/chatbot.jpg">
-        <span class= "new">Loading search results...</span>
+        <img src="../static/img/chatbot.jpg">
+        <span class= "new_${i}">Loading search results...</span>
       </div>`;
+        messageBox.insertAdjacentHTML("beforeend", message);
+        messageBox.insertAdjacentHTML("beforeend", response);
 
-    messageBox.insertAdjacentHTML("beforeend", message);
-    messageBox.insertAdjacentHTML("beforeend", response);
+        const requestOptions = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                "message": userTypedMessage
+            })
+        };
 
-    const requestOptions = {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({
-        "message": userTypedMessage 
-      })
-    };
+        try {
+            const res = await fetch(API_URL, requestOptions);
+            if (!res.ok) {
+                throw new Error('Failed to fetch data');
+            }
+            const data = await res.json();
+            const chatBotResponse = document.querySelector(".response .new_"+i);
+            if (!Array.isArray(data.results)) {
+                chatBotResponse.innerHTML = `<div class="card"><div class="card-body"><p>${data.results}</p></div></div>`
+            } else {
+                chatBotResponse.innerHTML = ''
+                const filteredResults = data.results.filter(result => result.title !== '' && result.description !== '').slice(0, 5);
+                filteredResults.forEach(result => {
+                    chatBotResponse.innerHTML += `<div class="card">
+                                          <div class="card-body">
+                                                   <h5><img src="${result.og_image}"/> ${result.title}</h5>
+                                                  <p class="card-text">${result.description}
+                                                  </p>
+                                                  
+                                              </div>
+                                        </div>`;
+                });
+                chatBotResponse.innerHTML += `<button class="btn-dark" onclick="navigateToExplore()">Explore</button>`
+            }
 
-    try {
-      const res = await fetch(API_URL, requestOptions);
-      if (!res.ok) {
-        throw new Error('Failed to fetch data');
-      }
-      const data = await res.json();
-      const chatBotResponse = document.querySelector(".response .new");
-      chatBotResponse.innerHTML = '';
-
-      data.results.forEach(result => {
-        chatBotResponse.innerHTML += `<div class="result">
-                                        <h3>${result.title}</h3>
-                                        <p>${result.description}</p>
-                                      </div>`;
-      });
-      chatBotResponse.classList.remove("new"); // Remove the "Loading search results..." message
-    } catch (error) {
-      console.error('Error:', error);
-      const chatBotResponse = document.querySelector(".response .new");
-      chatBotResponse.innerHTML = "Oops! An error occurred. Please try again";
-      chatBotResponse.classList.remove("new"); // Remove the "Loading search results..." message
+        i = i + 1;
+        } catch (error) {
+            console.error('Error:', error);
+            const chatBotResponse = document.querySelector(".response .new_"+i);
+            chatBotResponse.innerHTML = "Oops! An error occurred. Please try again";
+            document.querySelector('.new').remove();
+        }
     }
-  }
 };
+
+function navigateToExplore() {
+    window.location.href = "/result"
+}
 
 // Event listener for send button click
 sendBtn.onclick = sendMessage;
 
 // Event listener for Enter key press
-messageBar.addEventListener("keypress", function(event) {
-  if (event.key === "Enter") {
-    sendMessage();
-  }
+messageBar.addEventListener("keypress", function (event) {
+    if (event.key === "Enter") {
+        sendMessage();
+    }
 });
 
 // Function to trigger video source change after sending message
@@ -95,14 +108,14 @@ messageBar.addEventListener("keypress", function(event) {
 
 // Trigger video source change after sending message
 sendBtn.onclick = async () => {
-  await sendMessage();
-  // await triggerVideoSourceChange();
+    await sendMessage();
+    // await triggerVideoSourceChange();
 };
 
 // Trigger video source change after pressing Enter key
-messageBar.addEventListener("keypress", async function(event) {
-  if (event.key === "Enter") {
-    await sendMessage();
-    // await triggerVideoSourceChange();
-  }
+messageBar.addEventListener("keypress", async function (event) {
+    if (event.key === "Enter") {
+        await sendMessage();
+        // await triggerVideoSourceChange();
+    }
 });
