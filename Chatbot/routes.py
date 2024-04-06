48a4bd5d5c651f49d16e1d66faeff835da9f57a1
avni0405 <avinavekariya6@gmail.com>
2024-04-06 17:54:42 +0530

    Added video

--- Chatbot/routes.py
+++ Chatbot/routes.py
@@ -1,3 +1,6 @@
+import json
+
+from pytrends.request import TrendReq
 from quart import send_file, Blueprint, render_template
 import asyncio
 import aiohttp
@@ -13,8 +16,9 @@ import time
 from moviepy.editor import VideoFileClip, concatenate_videoclips
 import speech_recognition as sr
 from openai import OpenAI
-import pytrends
-from pytrends.request import TrendReq
+import os
+import requests
+from bs4 import BeautifulSoup
 
 bp = Blueprint("routes", __name__)
 
@@ -52,7 +56,7 @@ async def site_details(session, url):
                         'property' in meta.attrs and meta.attrs['property'] == 'og:image']
             og_image = og_image[0] if og_image else ""
 
-            title_with_link = f"• <b><a href='{url}' target='_blank'>{title}</a></b>"
+            title_with_link = f"<a href='{url}' target='_blank'>{title}</a>"
             return {'url': url, 'title': title_with_link, 'description': description, 'og_image': og_image}
         except Exception as e:
             print(f"Error parsing HTML from {url}: {e}")
@@ -69,63 +73,70 @@ async def search_google_async(query, num_results=10, lang="en"):
 
 @bp.route('/result', methods=['GET'])
 async def show_result():
-    result = {
-        "searches": [
-            {
-                "title": "wikipedia",
-                "description": "this is wikipedia"
-            },
-            {
-                "title": "wikipedia1",
-                "description": "this is wikipedia"
-            },
-            {
-                "title": "wikipedia2",
-                "description": "this is wikipedia"
-            },
-            {
-                "title": "wikipedia3",
-                "description": "this is wikipedia"
-            },
-        ]
-    }
-
-    return await render_template('results.html', result=result)
+    data = {}
+    with open("result.json", "r") as json_file:
+        data = json.load(json_file)
+    return await render_template('results.html', result=data)
 
 
 @bp.route('/search', methods=['POST'])
 async def search_google():
+    result = {}
     user_message = (await request.json)['message']
     if check_matching_phrase(user_message):
         results_with_snippets = "Hello! I am feeling wonderfull today. How can I assist you today?"
     else:
         results_with_snippets = await search_google_async(user_message)
+
+        # Save the results to an HTML file
+        var = ""
+        if len(results_with_snippets) > 0:
+            var = results_with_snippets[0]["description"]
+        summary = summarize_content(var)
+        result["summary"] = summary
+
+    result["searches"] = results_with_snippets
+    # result["graph"] = track_keyword_trends(user_message)
+    with open("result.json", "w") as json_file:
+        json.dump(result, json_file)
+
     # Return the JSON response
-    return jsonify({'results': results_with_snippets})
+    return jsonify({'results': result})
 
 
-async def save_results_to_html(results, user_message):
-    pass
-    # Create or open the HTML file for writing with UTF-8 encoding
-    # with open('results.html', 'w', encoding='utf-8') as f:
-    #     # Write the HTML content to the file
-    #     # user_message = (await request.json)['message']
-    #     title_res = "Your Search: " + user_message
-    #     f.write(
-    #         '<!DOCTYPE html>\n<html lang="en" data-bs-theme="dark">\n<head>\n<meta charset="UTF-8">\n<meta http-equiv="X-UA-Compatible" content="IE=edge">\n<meta name="viewport" content="width=device-width, initial-scale=1.0">\n<link rel="stylesheet" href="style.css">\n <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">\n<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />\n<link rel="preconnect" href="https://fonts.googleapis.com">\n<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>\n<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap" rel="stylesheet">\n<title>Chalaang Queens</title>\n<body><nav class="navbar">\n<a class="navbar-brand" href="#">\n<img src="img\logo.png" width="40" height="40" alt="">\n</a>\n<a class="navbar-brand" href="#"><b>Chalaang-Queens</b></a>\n<ul class="nav">\n<li class="nav-item">\n<a class="nav-link active" aria-current="page" href="index.html">Home</a>\n</li>\n<li class="nav-item">\n<a class="nav-link" href="#">Your Search</a>\n</li>\n</ul>\n</nav>\n<h1><center>{}</center></h1>\n'.format(
-    #             title_res))
-    #     for result in results:
-    #         f.write(
-    #             f"<li><a href='{result['url']}' target='_blank'>{result['title']}</a><br>{result['description']}</li>\n")
-    #     f.write('</ul>\n</body>\n</html>')
+def track_keyword_trends(keyword):
+    # Initialize pytrends
+    pytrends = TrendReq()
+
+    # Build Payload
+    pytrends.build_payload(kw_list=[keyword])
+
+    # Get Interest Over Time
+    interest_over_time_df = pytrends.interest_over_time()
+
+    # Extract x-axis and y-axis data
+    x_data_time = interest_over_time_df.index
+    y_data_time = interest_over_time_df.index.strftime('%Y-%m-%d').tolist()
+    plot_type = 'line'
+
+    # Get Interest by Region
+    interest_by_region_df = pytrends.interest_by_region()
+    top_regions = interest_by_region_df[keyword].sort_values(ascending=False).head(10)
+
+    # Extract x-axis and y-axis data for region chart
+    x_data_region = top_regions.index
+    y_data_region = top_regions.values
+
+    return [{'x1': x_data_time, 'label': 'Date', 'graph_type': plot_type, 'y1': y_data_time, 'label': 'Interest',
+             'graph_type': plot_type},
+            {'x2': x_data_region, 'label': 'Region', 'graph_type': 'bar', 'y2': y_data_region, 'label': 'Interest',
+             'graph_type': 'bar'}]
 
 
 def video_audio_overlay():
-    # Get the duration of the text-to-speech audio
-    #     greeting()
-    audio_path = r'img\testtemp.wav'
+    audio_path = r'static/img/testtemp.wav'
     audio = mp.AudioFileClip(audio_path)
-    video_path = r'img\fox.mp4'
+    video_path = r'static/img/fox.mp4'
     audio_duration = audio.duration
     video_clip = mp.VideoFileClip(video_path)
     print("vaishnavi")
@@ -140,25 +151,25 @@ def video_audio_overlay():
     WIN_WIDTH = 900
     WIN_HEIGHT = 700
     DISPLAY = (WIN_WIDTH, WIN_HEIGHT)
-    end_image_path = r'img\assistant2.png'  # Replace with the path to your image
+    end_image_path = r'static/img/assistant2.png'  # Replace with the path to your image
     end_image = mp.ImageClip(end_image_path).set_duration(0.1)
     end_image.resize(DISPLAY)
     video_clip1.resize(DISPLAY)
     video_clip1 = mp.concatenate_videoclips([video_clip1, end_image])
     video_clip1 = video_clip1.set_audio(audio)
-    output_path = r'C:\Chalaang\Chatbot\img\testvid2.mp4'
-    video_clip.write_videofile(output_path, codec='libx264', fps=30)
-    asyncio.create_task(change_video_src())
+    output_path = r'static/img/testvid2.mp4'
+    video_clip1.write_videofile(output_path, codec='libx264', fps=30)
+    # asyncio.create_task()
 
 
-async def change_video_src():
-    # JavaScript code to change the src attribute of the iframe
-    js_code = """
-    var iframe = document.getElementById('videoFrame');
-    iframe.src = 'img\testvid2.mp4';  // Change 'new_video.mp4' to the path of your new video file
-    """
+# async def change_video_src():
+#     # JavaScript code to change the src attribute of the iframe
+#     js_code = """
+#     var iframe = document.getElementById('videoFrame');
+#     iframe.src = 'img\testvid2.mp4';  // Change 'new_video.mp4' to the path of your new video file
+#     """
 
-    return js_code
+#     return js_code
 
 
 def assistant(audio):
@@ -167,7 +178,7 @@ def assistant(audio):
     voices = engine.getProperty('voices')
     engine.setProperty('voice', voices[1].id)
     engine.setProperty('rate', 130)
-    engine.save_to_file(audio, r'../static/img/testtemp.wav')
+    engine.save_to_file(audio, r'static/img/testtemp.wav')
     engine.runAndWait()
     video_audio_overlay()
     # Call the function to change video src
@@ -176,11 +187,12 @@ def assistant(audio):
 # Flask route to handle the request to change video src
 @bp.route('/change_video_src')
 async def change_video_src_handler():
-    user_message = (await request.json)['message']
-    results_with_snippets = await search_google_async(user_message)
-    assistant(results_with_snippets[0]['description'])
+    summary = request.args.get('message')
+    sentences = summary.split('. ')
+    first_three_sentences = '. '.join(sentences[:3])
+    assistant(first_three_sentences)
 
-    return await change_video_src()
+    return {}
 
 
 def check_matching_phrase(input_text):
@@ -201,17 +213,15 @@ def check_matching_phrase(input_text):
     return False
 
 
-def summarize_content(content):
+def summarize_content(var):
     client = OpenAI(
-        api_key="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",
+        api_key="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",
     )
-    prompt_text = [{"role": "system",
-                    "content": "You are a serach engine.Summarize/analyze webpages to give comprehensive information"},
-                   {"role": "user", "content": content}]
+
     response = client.completions.create(
         model="gpt-3.5-turbo-instruct",
-        prompt=prompt_text,
-        max_tokens=1000,  # Adjust the max tokens to control the length of the summary
+        prompt="Summarize text  in 5 to 10 crisp sentences " + var,
+        max_tokens=300,  # Adjust the max tokens to control the length of the summary
         temperature=0.6,  # Adjust the temperature for diversity in the generated text
         top_p=1.0,
         frequency_penalty=0.0,
--- Chatbot/static/js/script.js
+++ Chatbot/static/js/script.js
@@ -1,7 +1,7 @@
 const messageBar = document.querySelector(".bar-wrapper input");
 const sendBtn = document.querySelector(".bar-wrapper button");
 const messageBox = document.querySelector(".message-box");
-let i =0
+let i = 0
 const API_URL = "http://localhost:5000/search"; // Update with your Flask API endpoint
 
 // Function to send message
@@ -42,29 +42,49 @@ const sendMessage = async () => {
                 throw new Error('Failed to fetch data');
             }
             const data = await res.json();
-            const chatBotResponse = document.querySelector(".response .new_"+i);
-            if (!Array.isArray(data.results)) {
-                chatBotResponse.innerHTML = `<div class="card"><div class="card-body"><p>${data.results}</p></div></div>`
+
+            const chatBotResponse = document.querySelector(".response .new_" + i);
+            if (!Array.isArray(data.results.searches)) {
+                chatBotResponse.innerHTML = `<div class="card"><div class="card-body"><p>${data.results.searches}</p></div></div>`
             } else {
-                chatBotResponse.innerHTML = ''
-                const filteredResults = data.results.filter(result => result.title !== '' && result.description !== '').slice(0, 5);
+
+                chatBotResponse.innerHTML = `<div class="card">
+                    <div class="card-body"><h5>Summary: </h5><p>${data.results.summary}</p>
+                    </div></div>`
+
+                const filteredResults = data.results.searches.filter(result => result.title !== '' && result.description !== '').slice(0, 5);
                 filteredResults.forEach(result => {
                     chatBotResponse.innerHTML += `<div class="card">
                                           <div class="card-body">
                                                    <h5><img src="${result.og_image}"/> ${result.title}</h5>
                                                   <p class="card-text">${result.description}
                                                   </p>
-                                                  
                                               </div>
                                         </div>`;
                 });
                 chatBotResponse.innerHTML += `<button class="btn-dark" onclick="navigateToExplore()">Explore</button>`
+                fetch('/change_video_src?message=' + data.results.summary, {
+                    method: 'GET',
+                })
+                    .then(response => {
+                        if (!response.ok) {
+                            throw new Error('Network response was not ok');
+                        }
+                        // Get the iframe element by its ID
+                        var iframe = document.getElementById('videoFrame');
+
+                        iframe.src = '../static/img/testvid2.mp4'; // Replace 'new_video.mp4' with your desired video source
+
+                    })
+                    .catch(error => {
+                        console.error('There was a problem with the AJAX request:', error);
+                    });
             }
 
-        i = i + 1;
+            i = i + 1;
         } catch (error) {
             console.error('Error:', error);
-            const chatBotResponse = document.querySelector(".response .new_"+i);
+            const chatBotResponse = document.querySelector(".response .new_" + i);
             chatBotResponse.innerHTML = "Oops! An error occurred. Please try again";
             document.querySelector('.new').remove();
         }
