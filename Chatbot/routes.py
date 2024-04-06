59b493e3c139dc9d0563e59b8d2df90835f802d9
Ashwini1970 <102524864+Ashwini1970@users.noreply.github.com>
2024-04-06 16:59:33 +0530

    Update routes.py
    Signed-off-by: Ashwini1970 <102524864+Ashwini1970@users.noreply.github.com>

--- Chatbot/routes.py
+++ Chatbot/routes.py
@@ -1,3 +1,5 @@
+import json
+
 from quart import send_file, Blueprint, render_template
 import asyncio
 import aiohttp
@@ -13,8 +15,17 @@ import time
 from moviepy.editor import VideoFileClip, concatenate_videoclips
 import speech_recognition as sr
 from openai import OpenAI
-import pytrends
+import os
+import requests
+from bs4 import BeautifulSoup
 from pytrends.request import TrendReq
+import matplotlib.pyplot as plt
+import pandas as pd
+import os
+current_file_path = os.path.abspath(__file__)
+current_directory = os.path.dirname(current_file_path)
+current_directory=current_directory.replace("\\", "/")
+file_path=(current_directory + "/search_result.csv")
 
 bp = Blueprint("routes", __name__)
 
@@ -52,7 +63,7 @@ async def site_details(session, url):
                         'property' in meta.attrs and meta.attrs['property'] == 'og:image']
             og_image = og_image[0] if og_image else ""
 
-            title_with_link = f"• <b><a href='{url}' target='_blank'>{title}</a></b>"
+            title_with_link = f"<a href='{url}' target='_blank'>{title}</a>"
             return {'url': url, 'title': title_with_link, 'description': description, 'og_image': og_image}
         except Exception as e:
             print(f"Error parsing HTML from {url}: {e}")
@@ -66,58 +77,93 @@ async def search_google_async(query, num_results=10, lang="en"):
         tasks = [site_details(session, url) for url in search_results]
         return await asyncio.gather(*tasks)
 
+def track_keyword_trends(keyword):
+    # Initialize pytrends
+    pytrends = TrendReq()
+
+    # Build Payload
+    pytrends.build_payload(kw_list=[keyword])
+
+    # Get Interest Over Time
+    interest_over_time_df = pytrends.interest_over_time()
+
+    # Plot the trend over time
+    plt.figure(figsize=(10, 6))
+    interest_over_time_df[keyword].plot()
+    plt.title(f'Interest Over Time for "{keyword}" on Google Search')
+    plt.xlabel('Date')
+    plt.ylabel('Interest')
+    plt.grid(True)
+    plt.show()
+
+    # Extract x-axis and y-axis data
+    x_data_time = interest_over_time_df.index
+    y_data_time = interest_over_time_df[keyword]
+    plot_type = 'line'
+
+    # Get Interest by Region
+    interest_by_region_df = pytrends.interest_by_region()
+    top_regions = interest_by_region_df[keyword].sort_values(ascending=False).head(10)
+
+    # Plot the interest by region
+    plt.figure(figsize=(10, 6))
+    top_regions.plot(kind='bar', color='skyblue')
+    plt.title(f'Top 10 Regions Interested in "{keyword}" on Google Search')
+    plt.xlabel('Region')
+    plt.ylabel('Interest')
+    plt.xticks(rotation=45, ha='right')
+    plt.grid(axis='y')
+    plt.show()
+
+    # Extract x-axis and y-axis data for region chart
+    x_data_region = top_regions.index
+    y_data_region = top_regions.values
+
+    return [{'x1': x_data_time, 'label': 'Date', 'graph_type': plot_type ,'y1': y_data_time, 'label': 'Interest', 'graph_type': plot_type},{'x2': x_data_region, 'label': 'Region', 'graph_type': 'bar','y2': y_data_region, 'label': 'Interest', 'graph_type': 'bar'}]
+
+
 
 @bp.route('/result', methods=['GET'])
 async def show_result():
-    result = {
-        "searches": [
-            {
-                "title": "wikipedia",
-                "description": "this is wikipedia"
-            },
-            {
-                "title": "wikipedia1",
-                "description": "this is wikipedia"
-            },
-            {
-                "title": "wikipedia2",
-                "description": "this is wikipedia"
-            },
-            {
-                "title": "wikipedia3",
-                "description": "this is wikipedia"
-            },
-        ]
-    }
-
-    return await render_template('results.html', result=result)
+    data = {}
+    with open("result.json", "r") as json_file:
+        data = json.load(json_file)
+    return await render_template('results.html', result=data)
 
 
 @bp.route('/search', methods=['POST'])
 async def search_google():
+    result = {}
     user_message = (await request.json)['message']
     if check_matching_phrase(user_message):
         results_with_snippets = "Hello! I am feeling wonderfull today. How can I assist you today?"
     else:
         results_with_snippets = await search_google_async(user_message)
+
+        # Save the results to an HTML file
+        for i in results_with_snippets:
+            var = i["description"]
+        summary = summarize_content(var)
+        result["summary"] = summary
+
+    result["searches"] = results_with_snippets
+    result["graph"] = track_keyword_trends(user_message)
+    with open("result.json", "w") as json_file:
+        json.dump(result, json_file)
+
+    ## Saving the search result in a dataset
+    primary_key_column = 'url'
+    new_df = pd.DataFrame(results_with_snippets)
+    existing_data = pd.read_csv(file_path)
+    concatenated_data = pd.concat([existing_data, new_df], ignore_index=True)
+    concatenated_data.drop_duplicates(subset=[primary_key_column], keep='first', inplace=True)
+    concatenated_data.set_index(primary_key_column, inplace=True, drop=False)
+    concatenated_data.to_csv(file_path, index=False)
+
     # Return the JSON response
-    return jsonify({'results': results_with_snippets})
+    return jsonify({'results': result})
 
 
-async def save_results_to_html(results, user_message):
-    pass
-    # Create or open the HTML file for writing with UTF-8 encoding
-    # with open('results.html', 'w', encoding='utf-8') as f:
-    #     # Write the HTML content to the file
-    #     # user_message = (await request.json)['message']
-    #     title_res = "Your Search: " + user_message
-    #     f.write(
-    #         '<!DOCTYPE html>\n<html lang="en" data-bs-theme="dark">\n<head>\n<meta charset="UTF-8">\n<meta http-equiv="X-UA-Compatible" content="IE=edge">\n<meta name="viewport" content="width=device-width, initial-scale=1.0">\n<link rel="stylesheet" href="style.css">\n <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">\n<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />\n<link rel="preconnect" href="https://fonts.googleapis.com">\n<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>\n<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap" rel="stylesheet">\n<title>Chalaang Queens</title>\n<body><nav class="navbar">\n<a class="navbar-brand" href="#">\n<img src="img\logo.png" width="40" height="40" alt="">\n</a>\n<a class="navbar-brand" href="#"><b>Chalaang-Queens</b></a>\n<ul class="nav">\n<li class="nav-item">\n<a class="nav-link active" aria-current="page" href="index.html">Home</a>\n</li>\n<li class="nav-item">\n<a class="nav-link" href="#">Your Search</a>\n</li>\n</ul>\n</nav>\n<h1><center>{}</center></h1>\n'.format(
-    #             title_res))
-    #     for result in results:
-    #         f.write(
-    #             f"<li><a href='{result['url']}' target='_blank'>{result['title']}</a><br>{result['description']}</li>\n")
-    #     f.write('</ul>\n</body>\n</html>')
 
 
 def video_audio_overlay():
@@ -151,14 +197,14 @@ def video_audio_overlay():
     asyncio.create_task(change_video_src())
 
 
-async def change_video_src():
-    # JavaScript code to change the src attribute of the iframe
-    js_code = """
-    var iframe = document.getElementById('videoFrame');
-    iframe.src = 'img\testvid2.mp4';  // Change 'new_video.mp4' to the path of your new video file
-    """
+# async def change_video_src():
+#     # JavaScript code to change the src attribute of the iframe
+#     js_code = """
+#     var iframe = document.getElementById('videoFrame');
+#     iframe.src = 'img\testvid2.mp4';  // Change 'new_video.mp4' to the path of your new video file
+#     """
 
-    return js_code
+#     return js_code
 
 
 def assistant(audio):
@@ -178,7 +224,7 @@ def assistant(audio):
 async def change_video_src_handler():
     user_message = (await request.json)['message']
     results_with_snippets = await search_google_async(user_message)
-    assistant(results_with_snippets[0]['description'])
+    assistant(results_with_snippets['Summary'])
 
     return await change_video_src()
 
@@ -201,17 +247,15 @@ def check_matching_phrase(input_text):
     return False
 
 
-def summarize_content(content):
+def summarize_content(var):
     client = OpenAI(
-        api_key="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",
+        ,
     )
-    prompt_text = [{"role": "system",
-                    "content": "You are a serach engine.Summarize/analyze webpages to give comprehensive information"},
-                   {"role": "user", "content": content}]
+
     response = client.completions.create(
         model="gpt-3.5-turbo-instruct",
-        prompt=prompt_text,
-        max_tokens=1000,  # Adjust the max tokens to control the length of the summary
+        prompt="Summarize text  in 10 to 20 sentences " + var,
+        max_tokens=100,  # Adjust the max tokens to control the length of the summary
         temperature=0.6,  # Adjust the temperature for diversity in the generated text
         top_p=1.0,
         frequency_penalty=0.0,
